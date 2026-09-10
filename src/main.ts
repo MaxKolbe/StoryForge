a05f42b4052159447b624fbd70f90d266c74eb18
Maxmillian <maximilianogbuabor@gmail.com>
2026-09-10 23:01:48 +0100

    fix: add validation wiith class-validators and global Validationpipe

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import { env } from "./config/env.validation.js";
+import { env } from './config/env.validation.js';
 import { logger } from './config/logger.config.js';
 import { connectDatabase } from './config/db.config.js';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -13,8 +14,9 @@ async function bootstrap() {
   });
 
   app.useLogger(new logger());
-  app.useStaticAssets(join(process.cwd(), "/public"));
-  app.setBaseViewsDir(join(process.cwd(), "/views"));
+  app.useGlobalPipes(new ValidationPipe());
+  app.useStaticAssets(join(process.cwd(), '/public'));
+  app.setBaseViewsDir(join(process.cwd(), '/views'));
   app.setViewEngine('ejs');
 
   await connectDatabase();
--- src/modules/auth/dto/auth.dto.ts
+++ src/modules/auth/dto/auth.dto.ts
@@ -1,4 +1,9 @@
+import { Length, IsEmail, IsString } from 'class-validator';
 export class AuthDto {
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @Length(8, 16)
   password: string;
 }
